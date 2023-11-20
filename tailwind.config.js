75447a65f4ff874358d406e7656c6497ab4f67c1
Md. Shakib Mia <smdshakibmia2001@gmail.com>
2023-11-20 11:39:37 +0600

    tailwindcss config added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ["./index.html", "./*.js", "./public/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#CBFC01",
+        secondary: "#151515",
+        white: "#ffffff",
+        light: "#FFFFFFB2",
+        dark: {
+          DEFAULT: "#212121",
+          secondary: "#2E2E2E",
+        },
+        black: {
+          DEFAULT: "#000000",
+          secondary: "#000000b2",
+        },
+      },
+    },
   },
   plugins: [],
 };
