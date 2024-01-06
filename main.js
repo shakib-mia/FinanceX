c771d4235f560d3352e95fb26287f20a446537a2
Md. Shakib Mia <smdshakibmia2001@gmail.com>
2024-01-06 21:20:38 +0600

    changes according to suggestion are done

--- main.js
+++ main.js
@@ -1,13 +1,63 @@
+import Lenis from "@studio-freight/lenis";
 import "./style.css";
 
-fetch("./index.html")
-  .then((res) => res.text())
-  .then(
-    (htmlContent) => (document.querySelector("#app").innerHTML = htmlContent)
-  );
+const lenis = new Lenis();
+
+lenis.on("scroll", (e) => {
+  // console.log(e);
+});
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
 
 // function toggleNavbar() {
 //   var navbarMenu = document.querySelector(".navbar-menu");
 //   navbarMenu.style.display =
 //     navbarMenu.style.display === "flex" ? "none" : "flex";
 // }
+
+$(document).ready(function () {
+  // slider for tutorial section
+  $("#testimonial .owl-carousel").owlCarousel({
+    margin: 20,
+    dots: false,
+    nav: true,
+    // center: true,
+    startPosition: 1,
+    responsive: {
+      0: {
+        items: 1,
+      },
+
+      768: {
+        items: 2,
+      },
+
+      1024: {
+        items: 3,
+      },
+    },
+  });
+});
+
+const handleSlideClick = (id, direction) => {
+  document.querySelector(id + ` .owl-${direction}`).click();
+};
+
+document
+  .getElementById("owl-next")
+  .addEventListener("click", () =>
+    handleSlideClick("#testimonial-slider", "next")
+  );
+
+document
+  .getElementById("owl-prev")
+  .addEventListener("click", () =>
+    handleSlideClick("#testimonial-slider", "prev")
+  );
+
+document.getElementById("year").innerText = new Date().getFullYear();
